4fa87fd0879b5fb41d05140c22940a5b5f86b927
lunchbird <alex.a.kontos@gmail.com>
2023-01-30 19:17:18 +0200

    Complete helper fn and ut step3 through step5

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,4 +1,5 @@
 const dummy = (blogs) => 1;
+const _ = require('lodash');
 
 const totalLikes = (blogs) => {
   return blogs.reduce((acc, blog) => {
@@ -6,4 +7,49 @@ const totalLikes = (blogs) => {
   }, 0)
 }
 
-module.exports = { dummy, totalLikes };
+const favoriteBlog = (blogs) => {
+  let fav;
+  blogs.reduce((acc, blog) => {
+    if (blog.likes > acc.likes) {
+      fav = blog;
+      return blog
+    } else {
+      fav = acc;
+      return acc;
+    }
+  })
+  let { _id, url, __v, ...returnValue } = fav;
+  return returnValue;
+}
+
+const mostBlogs = (blogs) => {
+  const mostBloggingAuthor = _.chain(blogs)
+    .groupBy("author")
+    .map((group, author) => {
+      return {
+        author: author,
+        blogs: group.length
+      }
+    })
+    .maxBy((author) => author.blogs)
+    .value()
+
+  return mostBloggingAuthor
+}
+
+const mostLikes = (blogs) => {
+  const mostLikedAuthor = _.chain(blogs) // begins a chain with blogs
+    .groupBy("author") // creates an object with each author as key, inside their blogs as objects in array
+    .map((group, author) => { // group is array of blog objects, author is the key
+      return { // for each key, return this to replace it
+        author: author, // the author key as author property
+        likes: _.sumBy(group, blog => blog.likes)
+      }
+    })
+    .maxBy((author) => author.likes) // find max value
+    .value() // dewrap chain
+
+  return mostLikedAuthor
+}
+
+module.exports = { dummy, totalLikes, favoriteBlog, mostBlogs, mostLikes };
